efecb75c1c0794dd0b8504d76c447dd482c3f1b8
laliswamyboya533-cloud <laliswamyboya533@gmail.com>
2025-10-20 12:28:07 +0530

    Add code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// Hoc
+//Higher order Component is a function that takes a component and return a new compoenent
+// Example loader with list compoenent
 
-function App() {
+import React, { useState, useEffect } from 'react';
+
+export const HOC = (Component) => {
+  const Newcomponent = ({ loading, ...rest }) => {
+    if (loading) return <div>Loading...</div>;
+    return <Component {...rest} />;
+  };
+  return Newcomponent;
+};
+
+export const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoader] = useState(true);
+  const WrapperComponent = HOC(UserList);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/users')
+      .then((res) => res.json())
+      .then((o) => {
+        setData(o.users);
+        setTimeout(() => {
+          setLoader(false);
+        }, 300);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WrapperComponent loading={loading} user={data} />
     </div>
   );
-}
+};
 
-export default App;
+export const UserList = ({ user }) => {
+  return (
+    <div>
+      {user.map((x, index) => {
+        return <div key={index}>{x.firstName}</div>;
+      })}
+    </div>
+  );
+};
